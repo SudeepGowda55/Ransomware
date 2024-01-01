dbc94249e918b3f5341fd84fbd530cdfd4a4c551
SudeepGowda55 <ssudeepgowda55@gmail.com>
2024-01-01 22:44:46 +0530

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::env;
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Aree Buddy execute the file like cargo run encrypt/decrypt folder-path");
+        return;
+    }
+
     println!("{:#?}", args);
 }
